09ec196959891c73774c52c4e136f7b3bbc34945
wwilliamwai <wongwilliam102@gmail.com>
2025-08-22 23:41:03 -0700

    ready message for patch

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.user.setActivity('probably being worked on', { type: ActivityType.Custom, state: 'double down added' });
+		client.user.setActivity('probably being worked on', { type: ActivityType.Custom, state: 'remove scaling from wages' });
 
 		// setup a way to type in general chat through the vscode terminal
 		const rl = readline.createInterface({
